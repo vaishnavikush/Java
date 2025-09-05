993089db419ebad6e388de9891903f5aca0cf90b
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2025-09-05 23:14:34 +0530

    Add files via upload

---
+++ Day3Package/AssignmentOperators.java
@@ -0,0 +1,15 @@
+package Day3Package;
+
+public class AssignmentOperators {
+    public static void main(String[] args){
+        int a=1;
+         a +=2;
+         System.out.println(a);
+         a *=2;
+         System.out.println(a);
+         a -=1;
+         System.out.println(a);
+         a /=1;
+         System.out.println(a);
+    }
+}
