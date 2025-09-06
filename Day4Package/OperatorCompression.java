f44921005aa02f5b2e722135776ae8f22ed7117a
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2025-09-06 23:54:48 +0530

    Add files via upload

---
+++ Day4Package/OperatorCompression.java
@@ -0,0 +1,17 @@
+package Day4Package;
+import java.util.Scanner;
+public class OperatorCompression {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Value");
+        int user=sc.nextInt();
+        System.out.println("Checking whether a giving no. is greater than th user entered no. or not");
+        int define=20;
+        System.out.println(define>user);
+
+//        int v=2,a=5,s=7;
+//        int u=4;
+//        float f=(v*v-u*u)/(2*a*s);
+//        System.out.println(f);
+    }
+}
