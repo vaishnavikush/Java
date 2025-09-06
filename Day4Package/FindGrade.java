9bc48f81b6ec10a4621b57ff9c09b9c36a49c82e
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2025-09-06 23:54:27 +0530

    Add files via upload

---
+++ Day4Package/FindGrade.java
@@ -0,0 +1,18 @@
+package Day4Package;
+
+public class FindGrade {
+    public static void main(String[] args){
+        char grade='C';
+        System.out.println("Encrypting The Grade");
+        grade=(char) (grade+8);
+        System.out.println(grade);
+
+        System.out.println("Decrypting The Grade");
+        grade=(char) (grade-8);
+        System.out.println(grade);
+
+        float a=7/4.0f*9/2.0f;
+        System.out.println(a);
+
+    }
+}
