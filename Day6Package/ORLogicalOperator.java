3c95895795da5bfcae823716d57cd6ed35fab378
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2025-09-08 23:49:07 +0530

    Add files via upload

---
+++ Day6Package/ORLogicalOperator.java
@@ -0,0 +1,15 @@
+package Day6Package;
+
+public class ORLogicalOperator {
+    public static void main(String[] args){
+        boolean a= false;
+        boolean b= true;
+        boolean c= false;
+        if (a || b ||c) {
+            System.out.println("One Is true");
+        }
+        else{
+            System.out.println("No One Is True");
+        }
+    }
+}
