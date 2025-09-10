1e596a6ceadccafdf3a1e66257528239398dbd6e
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2025-09-10 23:51:08 +0530

    Add files via upload

---
+++ Day8Package/ForLoop.java
@@ -0,0 +1,19 @@
+package Day8Package;
+import java.util.Scanner;
+public class ForLoop {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("For Loop");
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+        System.out.println("Enter no");
+        int n=sc.nextInt();
+        for(int i=1; i<=n; i++){
+            int odd=i;
+           if(odd%2!=0){
+               System.out.println(odd);
+           }
+        }
+    }
+}
