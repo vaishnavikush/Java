db509c7f9ce958930fec22c4409c48dd9d11e9e6
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2025-09-11 23:30:16 +0530

    Add files via upload

---
+++ Day9Package/OddEven.java
@@ -0,0 +1,19 @@
+package Day9Package;
+
+public class OddEven {
+    public static void main(String [] args){
+        System.out.println("Another Logic For Odd Numbers");
+        int i;
+        for(i=0;i<5;i++){
+            int odd1=2*i+1;
+            System.out.println(odd1);
+        }
+
+        System.out.println("Another Logic For Even Numbers");
+        int y;
+        for(y=1;y<=5;y++){
+            int even =2*y;
+            System.out.println(even);
+        }
+    }
+}
