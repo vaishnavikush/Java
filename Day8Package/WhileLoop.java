3d7a0a189d6867d09a46b9a302c8b7a83a945079
[vaishnavikush] <vaishnavikushwaha53@gmail.com>
2025-09-13 10:00:00 +0530

    added

---
+++ Day8Package/WhileLoop.java
@@ -0,0 +1,23 @@
+package Day8Package;
+
+public class WhileLoop {
+    public static void main(String[] args){
+        System.out.println("Without using Loop");
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println("By Using While Loop");
+        int i=1;
+        while(i<=3){
+            System.out.println(i);
+            i++;
+        }
+//        System.out.println("For Making Infinite Loop");
+//        boolean true;
+//        while(true){
+//            System.out.println("Hola....");
+//        }
+
+
+    }
+}
