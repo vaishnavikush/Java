d0f14d16935fc13bde9770ecd41c26dc95d4706f
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2025-09-14 23:39:13 +0530

    Add files via upload

---
+++ Day12Package/ForEachLoop.java
@@ -0,0 +1,12 @@
+package Day12Package;
+
+public class ForEachLoop {
+    public static void main(String[] args){
+        System.out.println("Displaying Array Elements By Using For-Each Loop");
+        int []array={1,2,3,4,5};
+        for(int element:array){
+            System.out.println(element);
+        }
+    }
+}
+
