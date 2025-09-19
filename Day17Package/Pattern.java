742f6cc5ee53828d4c0c86673fc1c4bf35c98549
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2025-09-19 23:51:54 +0530

    Add files via upload

---
+++ Day17Package/Pattern.java
@@ -0,0 +1,18 @@
+package Day17Package;
+
+public class Pattern {
+    void Pattern1(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        Pattern pt=new Pattern();
+        System.out.println("Pattern In Increasing Order");
+            pt.Pattern1(5);
+    }
+}
