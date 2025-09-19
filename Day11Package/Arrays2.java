eb3235aa3c5ad3de619497a645eacdec6a6dd755
[vaishnavikush] <vaishnavikushwaha53@gmail.com>
2025-09-19 10:00:00 +0530

    added

---
+++ Day11Package/Arrays2.java
@@ -0,0 +1,26 @@
+package Day11Package;
+
+public class Arrays2 {
+    public static void main(String[] args){
+        System.out.println("Array By Using For Loop");
+        int []arr={1,2,34,4,5};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+            System.out.println("Array By Using While Loop");
+            int []arr1={1,2,34,4,5};
+            int y=0;
+            while(y<arr1.length){
+                System.out.println(arr1[y]);
+                y++;
+        }
+        System.out.println("Array By Using While Loop");
+        int []arr2={1,2,34,4,5};
+        int x=0;
+        do{
+            System.out.println(arr2[x]);
+            x++;
+        }while(x<arr2.length);
+}
+        }
+
