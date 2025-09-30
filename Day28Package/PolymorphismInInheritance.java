a48f024cd1c33e73bf3250dc0aeabe5f5e93a47e
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2025-09-30 12:43:21 +0530

    Add files via upload

---
+++ Day28Package/PolymorphismInInheritance.java
@@ -0,0 +1,39 @@
+package Day28Package;
+
+interface Name{
+    void setName(String name);
+    void getName();
+}
+interface Class{
+    void setClass(int Class);
+    void GetClass();
+}
+class age implements Name,Class{
+    String name;
+    int Class;
+  public void setName(String name){
+       this.name=name;
+   }
+   public void getName(){
+      System.out.println("Name : "+name);
+   }
+   public void setClass(int Class){
+      this.Class=Class;
+   }
+   public void GetClass(){
+      System.out.println("Class : "+Class);
+   }
+}
+public class PolymorphismInInheritance {
+    public static void main(String [] args){
+        Name nm=new age();
+        nm.setName("Kuch");
+        nm.getName();
+        System.out.println("------------");
+        age ag=new age();
+        ag.setName("Pata");
+        ag.getName();
+        ag.setClass(12);
+        ag.GetClass();
+    }
+}
