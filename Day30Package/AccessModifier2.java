b5fbb15aba572c2ac8534825515c49c1d5489e72
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2025-10-02 22:46:44 +0530

    Add files via upload

---
+++ Day30Package/AccessModifier2.java
@@ -0,0 +1,33 @@
+package Day30Package;
+
+class modifier{
+    private int m=10;
+    public int s=0;
+    protected int ds=40;
+    int x=80;
+    void Own(){
+        System.out.println("Private is :"+m);
+        System.out.println("Public is :"+s);
+        System.out.println("Protected is :"+ds);
+        System.out.println("Default is :"+x);
+    }
+}
+class access extends modifier{
+    void call(){
+        //System.out.println("Private is :"+m);//we cannot call private modifier outside the class
+        System.out.println("Public is :"+s);
+        System.out.println("Protected is :"+ds);
+        System.out.println("Default is :"+x);
+    }
+}
+public class AccessModifier2 {
+    public static void main(String[] args){
+        System.out.println("Access From The Same Class");
+        modifier on=new access();
+        on.Own();
+        System.out.println("-----------------");
+        System.out.println("Access From The Same Class");
+        access ac=new access();
+        ac.call();
+    }
+}
