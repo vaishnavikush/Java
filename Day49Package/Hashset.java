7c51168b8e742cf53d5cb2566f490713e83838dd
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2025-10-21 23:42:44 +0530

    Add files via upload

---
+++ Day49Package/Hashset.java
@@ -0,0 +1,18 @@
+package Day49Package;
+import java.util.*;
+public class Hashset {
+    public static void main(String[] args){
+        HashSet<Integer>hash1=new HashSet<>();
+        hash1.add(45);
+        hash1.add(23);
+        hash1.add(43);
+        hash1.add(78);
+        System.out.println(hash1);
+        HashSet<Integer>hash2=new HashSet<>(6,0.5f);
+        hash2.add(456);
+        hash2.add(243);
+        hash2.add(437);
+        hash2.add(678);
+        System.out.println(hash2);
+    }
+}
