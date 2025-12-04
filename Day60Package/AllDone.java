85f83bc93e324c6800d66e489b469ee635af19fa
[vaishnavikush] <vaishnavikushwaha53@gmail.com>
2025-12-04 10:00:00 +0530

    added

---
+++ Day60Package/AllDone.java
@@ -0,0 +1,9 @@
+package Day60Package;
+
+public class AllDone {
+    public static void main(String[] args){
+        for(int i=0;i<60;i++){
+            System.out.println("Oye hoye Java complete hoo giya");
+        }
+    }
+}
